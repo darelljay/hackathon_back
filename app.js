2d573279b961ff612485e4e99f5ac78a64cb9d81
darelljay <darelljasondj@gmail.com>
2023-07-27 18:05:39 +0900

    ...

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
-app.use(cors({credentials:true,origin:'http://localhost:3000/'}))
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/",router);
 
