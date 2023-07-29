004dd1c60fcb54743cfa983f896e78a5d297c2d6
darelljay <darelljasondj@gmail.com>
2023-07-29 12:20:35 +0900

    session logout complete

--- app.js
+++ app.js
@@ -4,13 +4,23 @@ const router = require("./router/router");
 const app = express();
 const cors = require('cors');
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized:true
+  })
+);
 
 app.use(bodyParser.json());
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/",router);
-
-
 app.listen(process.env.PORT || 8080,async (req, res) => {
     console.log(`server is runnig on port ${process.env.PORT || 8080}`);
   });
--- router/router.js
+++ router/router.js
@@ -1,93 +1,93 @@
-const express = require('express');
-const { militeryScrape, signUp, statusCodeFunction, findUsers, signIn, Registration, Login } = require('../controller/controller');
+const express = require("express");
+const {
+  militeryScrape,
+  signUp,
+  statusCodeFunction,
+  findUsers,
+  signIn,
+  Registration,
+  Login,
+} = require("../controller/controller");
 const router = express.Router();
-const session = require("express-session");
-const app = express();
+
 require("dotenv").config();
 
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie:{
-        httpOnly:true,
-    },
-    name:'connect.sid'
- }));
- 
- const auth = (req, res) => {
-    if (session && session.id) {
-      return session.UserInfo;
-    } else {
-      return 401;
+
+
+router.post("/scrape1", async (req, res) => {
+  const { url, selector } = req.body;
+
+  res.send(JSON.stringify(await militeryScrape(url, selector)));
+});
+
+router.post("/registration", async (req, res) => {
+  const { name, id, password, email } = req.body;
+  const registration = await Registration(name, id, password, email).catch(
+    (PromiseRejectionEvent) => {
+      PromiseRejectionEvent;
     }
+  );
+
+  if (registration === "Already Existing User.") {
+    res.status(400).json(registration);
+  } else if (registration === "Successfully Registered.") {
+    res.status(200).json(registration);
+    req.session.name = name;
+    req.session.Userid = id;
+    req.session.email = email;
+  } else {
+    res.status(500).json(registration);
   }
-  
-router.post('/scrape1',async (req,res)=>{
-    const {url,selector} = req.body;
-
-    res.send(JSON.stringify(await militeryScrape(url,selector)));
 });
 
-router.post('/registration',async(req,res)=>{
+router.post("/Login", async (req, res) => {
+  const { id, password } = req.body;
+  const login = await Login(id, password).catch((PromiseRejectionEvent) => {
+    PromiseRejectionEvent;
+  });
 
-    const {name,id,password,email} = req.body;
-    const registration = await Registration(name,id,password,email).catch(PromiseRejectionEvent=>{PromiseRejectionEvent});
+  if (login === "User Not Found." || login === "Wrong Password.") {
+    res.status(400).json(login[0]);
+  } else if (login[0] === "Successfully Loged In.") {
+      req.session.name = login[1].name;
+      req.session.Userid = login[1].id;
+      req.session.email = login[1].email;
+    
+      req.session.save(()=>{
+          res.status(200).json(login[0]);
+      });
 
-    if(registration === "Already Existing User."){
-        res.status(400).json(registration);
-    }else if(registration === "Successfully Registered."){
-        res.status(200).json(registration);
-        session.Info = {
-            name:name,
-            id:id,
-            password:password,
-            email:email
+  } else if (login === "Internal Server Error.") {
+    res.status(500).json("Internal Server Error");
+  } else {
+    console.error(login);
+  }
+});
+
+router.post("/Logout", (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+          console.log(err);
         }
-    }else{
-        res.status(500).json(registration);
-    }
+     });
+  res.status(200).json("Successfully Loged out");
 });
 
-router.post('/Login',async(req,res)=>{
-    const {id,password} = req.body;
-    const login = await Login(id,password).catch(PromiseRejectionEvent=>{PromiseRejectionEvent});
-   
-    if(login === "User Not Found." || login === "Wrong Password."){
-        res.status(400).json(login[0]);
-    }else if(login[0] === "Successfully Loged In."){
-        res.status(200).json(login[0]);
-        session.Info ={
-            name:login[1].name,
-            id:login[1].id,
-            email:login[1].email,
-            password:login[1].password
-        }
-    }else if (login === "Internal Server Error."){
-        res.status(500).json("Internal Server Error");
-    }else{
-        console.error(login)
-    }
+router.get("/", (req, res) => {
+  res.status(200).json("ok");
 });
 
-router.post('/logout',(req,res)=>{
-    req.session.destroy()
-    res.status(200).json("Successfully Loged out")
-            
-      
-})
-
-router.get('/',(req,res)=>{
-    res.status(200).json('ok');
-});
-
-router.get('/auth',(req,res)=>{
-    if(session.Info === undefined){
-        res.status(409).json('User Not Autherized')
+router.get("/auth", (req, res) => {
+    if(req.session.name !== undefined){
+        const UserInfo = {
+            name: req.session.name,
+            id: req.session.Userid,
+            email: req.session.email,
+          };
+          res.status(200).send(UserInfo);
     }else{
-        res.status(200).send(session.Info);
+        res.status(409).json("User Not Autherized");
     }
 });
 
 module.exports = router;
-
